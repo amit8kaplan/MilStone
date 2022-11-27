b8138a456f81f75eea74dfde55e950195c8707b9
amit8kaplan <amit.y.kaplan@gmail.com>
2022-11-27 22:40:37 +0200

    UPDATE

--- test/Board.java
+++ test/Board.java
@@ -1,10 +1,15 @@
 package test;
 
 
+import org.omg.CORBA.UNKNOWN;
+
 import java.util.ArrayList;
 
 public class Board {
     private static Board _instanceBoard = null;
+
+
+
     public static Board getBoard(){
         if(_instanceBoard==null)
             _instanceBoard=new Board();
@@ -14,15 +19,22 @@ public class Board {
     static int tileOnBoard =0;
     public Tile[][] boardGame;
     public Boolean[][] boardGameBoolean;
+    public ArrayList<Word> allWords;
+    private int[][] PiBoard = new int[15][15];
 
-    private Board(Tile[][] boardGame, Boolean[][] boardGameBoolean) {
-        this.boardGame = boardGame;
-        this.boardGameBoolean = boardGameBoolean;
-    }
     private Board(){
         this.boardGame = new Tile[15][15];
         this.boardGameBoolean= new Boolean[15][15];
+        this.allWords = new ArrayList<Word>();
+        PiBoard={[3,0,0,2,0,0,0,3,0,0,0,2,0,0,3],
+        };
 
+    }
+    private Board(Tile[][] boardGame, Boolean[][] boardGameBoolean, ArrayList<Word> allWords) {
+        this.boardGame = boardGame;
+        this.boardGameBoolean = boardGameBoolean;
+        this.allWords = new ArrayList<Word>();
+        this.allWords.addAll(allWords);
     }
 
     public Tile[][] getBoardGame() {
@@ -36,7 +48,11 @@ public class Board {
     public Tile[][] getTiles(){
         return getBoardGame();
     }
-
+    public boolean cheackIfFirstPositionIsOut (int row, int col){
+        if (col<0 || row>14||row<0 || row>14)
+            return false;
+        return true;
+    }
     public boolean checkifStar (int distance, int on, int Wlength)
     {
         if (distance!=7)
@@ -121,11 +137,14 @@ public class Board {
         return false;
     }
     public boolean boardLegal(Word w){
+        if (!cheackIfFirstPositionIsOut (w.getRow(), w.getCol()))
+            return false;
+
         //false - nothing there
         boolean checkCircle = false;
         //valid if w is more extend then the board
         if(w.isVertical()){
-            if((15-w.getCol())<w.getTiles().length)
+            if((15-w.getRow())<w.getTiles().length)
                 return false;
             if (tileOnBoard==0) {
                 if (!checkifStar(w.getCol(), w.getRow(), w.getTiles().length))
@@ -165,7 +184,7 @@ public class Board {
 //                    checkCircle=true;
         }
         else {
-            if ((15 - w.getRow()) < w.getTiles().length)
+            if ((15 - w.getCol()) < w.getTiles().length)
                 return false;
             if (tileOnBoard == 0) {
                 if (!checkifStar(w.getRow(), w.getCol(), w.getTiles().length))
@@ -212,19 +231,148 @@ public class Board {
         if (tileOnBoard==0){
             return myWords;
         }
-        for (int i=0; i<w.getTiles().length;i++)
-            if()
-        if (w.isVertical())
-        {
+
+        Tile[] t;
+        boolean untilTheEndOfTheRowStart= true;
+        int indexStart;
+        int indexEnd;
+        int indexUp;
+        int indexDown;
+        boolean untilTheEndOfTheRowEnd=true;
+        boolean untilTheEndOfTheColStart=true;
+        boolean untilTheEndOfTheColEnd=true;
+        if (w.isVertical()){
+            for (int i=w.getRow(); i<(w.getTiles().length+w.getRow()); i++){
+                untilTheEndOfTheRowStart=true;
+                untilTheEndOfTheRowEnd=true;
+                indexStart=1;
+                indexEnd=1;
+                if (this.boardGame[i][w.getCol()]==null) {
+                    while ((w.getCol()-indexStart)>=0 || untilTheEndOfTheRowStart) {
+                        if (this.boardGame[i][w.getCol()-indexStart] ==null)
+                            untilTheEndOfTheRowStart=false;
+                        else
+                            indexStart++;
+                    }
+                    while ((w.getCol()+indexEnd)<=14 || untilTheEndOfTheRowEnd) {
+                        if (this.boardGame[i][w.getCol()+indexEnd] ==null)
+                            untilTheEndOfTheRowEnd=false;
+                        else
+                            indexEnd++;
+                    }
+                    if (indexEnd==1)
+                        indexEnd=0;
+                    if (indexStart==1)
+                        indexStart=0;
+                    if (indexEnd+indexStart>0){
+                        t=new Tile[indexStart+indexEnd-1];
+                        for(int j=w.getCol()-indexStart+1; j<=w.getCol()+indexEnd-1; j++)
+                            t[j]= boardGame[i][j];
+                        myWords.add(new Word(t,i,w.getCol()-indexStart+1, false));
+                    }
+                }
+            }
+            untilTheEndOfTheColStart=true;
+            untilTheEndOfTheColEnd=true;
+            indexUp=0;
+            indexDown=0;
+            while ((w.getRow()-indexUp-1)>=0 || untilTheEndOfTheColStart)
+            {
+                if (this.boardGame[w.getRow()-indexUp-1][w.getCol()] ==null)
+                    untilTheEndOfTheColStart=false;
+                else
+                    indexUp++;
+            }
+            while ((w.getTiles().length+w.getRow()+indexDown-1)<=14 || untilTheEndOfTheColEnd)
+            {
+                if (this.boardGame[w.getTiles().length+w.getRow()+indexDown-1][w.getCol()] ==null)
+                    untilTheEndOfTheColEnd=false;
+                else
+                    indexDown++;
+            }
+            if (indexDown+indexUp>0){
+                t=new Tile[indexUp+w.getTiles().length+indexDown];
+                for(int i=w.getRow()-indexUp; i<w.getTiles().length+indexUp+indexDown; i++)
+                    t[i]=boardGame[i][w.getCol()];
+                myWords.add(new Word(t,w.getRow()-indexUp,w.getCol(),true));
+            }
+        }
+        else {
+            for (int i=w.getCol(); i<(w.getTiles().length+w.getCol()); i++){
+                untilTheEndOfTheRowStart=true;
+                untilTheEndOfTheRowEnd=true;
+                indexStart=1;
+                indexEnd=1;
+                if (this.boardGame[w.getRow()][i]==null) {
+                    while ((w.getRow()-indexStart)>=0 || untilTheEndOfTheRowStart) {
+                        if (this.boardGame[w.getRow()-indexStart][i] ==null)
+                            untilTheEndOfTheRowStart=false;
+                        else
+                            indexStart++;
+                    }
+                    while ((w.getRow()+indexEnd)<=14 || untilTheEndOfTheRowEnd) {
+                        if (this.boardGame[w.getRow()+indexEnd][i] ==null)
+                            untilTheEndOfTheRowEnd=false;
+                        else
+                            indexEnd++;
+                    }
+                    if (indexEnd==1)
+                        indexEnd=0;
+                    if (indexStart==1)
+                        indexStart=0;
+                    if (indexEnd+indexStart>0){
+                        t=new Tile[indexStart+indexEnd-1];
+                        for(int j=w.getRow()-indexStart+1; j<=w.getRow()+indexEnd-1; j++)
+                            t[j]= boardGame[i][j];
+                        myWords.add(new Word(t,i,w.getRow()-indexStart+1, true));
+                    }
+                }
+            }
+            untilTheEndOfTheColStart=true;
+            untilTheEndOfTheColEnd=true;
+            indexUp=0;
+            indexDown=0;
+            while ((w.getCol()-indexUp-1)>=0 || untilTheEndOfTheColStart)
+            {
+                if (this.boardGame[w.getRow()][w.getCol()-indexUp-1] ==null)
+                    untilTheEndOfTheColStart=false;
+                else
+                    indexUp++;
+            }
+            while ((w.getTiles().length+w.getCol()+indexDown-1)<=14 || untilTheEndOfTheColEnd)
+            {
+                if (this.boardGame[w.getRow()][w.getTiles().length+w.getCol()+indexDown-1] ==null)
+                    untilTheEndOfTheColEnd=false;
+                else
+                    indexDown++;
+            }
+            if (indexDown+indexUp>0){
+                t=new Tile[indexUp+w.getTiles().length+indexDown];
+                for(int i=w.getCol()-indexUp; i<w.getTiles().length+indexUp+indexDown; i++)
+                    t[i]=boardGame[w.getRow()][i];
+                myWords.add(new Word(t,w.getRow()-indexUp,w.getCol(),false));
+            }
 
         }
         return myWords;
     }
-
-
-
+    public boolean checkIfPi2(row)
+    public int getScore(Word w){
+        int sum =0;
+        //sum the word
+        for(t:w.getTiles())
+        {
+            sum+=
+        }
+        return sum;
+    }
 
 }
+
+
+
+
+
 
 
 
--- test/Tile.java
+++ test/Tile.java
@@ -6,11 +6,12 @@ import java.util.Random;
 
 public class Tile {
 
-    static int CountTileInArray =-1;
+    static int CountTileInArray =98;
     //todo: generate ctor with valid to letter
     public static class Bag{
         int[] LettersGame;
         Tile[] arrayTile;
+
 
         private static Bag _intance = null;
         public static Bag getBag(){
@@ -21,7 +22,63 @@ public class Tile {
         }
         private Bag(){
             this.LettersGame = new int[26];
+            this.LettersGame[0] = 9;
+            this.LettersGame[1] =2 ;
+            this.LettersGame[2] = 2;
+            this.LettersGame[3] =4 ;
+            this.LettersGame[4] =12 ;
+            this.LettersGame[5] = 2;
+            this.LettersGame[6] =3 ;
+            this.LettersGame[7] = 2;
+            this.LettersGame[8] = 9;
+            this.LettersGame[9] = 1;
+            this.LettersGame[10] =1 ;
+            this.LettersGame[11] =4 ;
+            this.LettersGame[12] =2 ;
+            this.LettersGame[13] = 6;
+            this.LettersGame[14] = 8;
+            this.LettersGame[15] = 2;
+            this.LettersGame[16] = 1;
+            this.LettersGame[17] = 6;
+            this.LettersGame[18] =4 ;
+            this.LettersGame[19]= 6;
+            this.LettersGame[20] =4 ;
+            this.LettersGame[21] =2 ;
+            this.LettersGame[22] = 2;
+            this.LettersGame[23] =1 ;
+            this.LettersGame[24] = 2;
+            this.LettersGame[25] =1 ;
+
+
+
             this.arrayTile= new Tile[26];
+            this.arrayTile[0]= new Tile('A',1);
+            this.arrayTile[1]= new Tile('B',3);
+            this.arrayTile[2]= new Tile('C',3);
+            this.arrayTile[3]= new Tile('D',2);
+            this.arrayTile[4]= new Tile('E',1);
+            this.arrayTile[5]= new Tile('F',4);
+            this.arrayTile[6]= new Tile('G',2);
+            this.arrayTile[7]= new Tile('H',4);
+            this.arrayTile[8]= new Tile('I',1);
+            this.arrayTile[9]= new Tile('J',8);
+            this.arrayTile[10]= new Tile('K',5);
+            this.arrayTile[11]= new Tile('L',1);
+            this.arrayTile[12]= new Tile('M',3);
+            this.arrayTile[13]= new Tile('N',1);
+            this.arrayTile[14]= new Tile('O',1);
+            this.arrayTile[15]= new Tile('P',3);
+            this.arrayTile[16]= new Tile('Q',10);
+            this.arrayTile[17]= new Tile('R',1);
+            this.arrayTile[18]= new Tile('S',1);
+            this.arrayTile[19]= new Tile('T',1);
+            this.arrayTile[20]= new Tile('U',1);
+            this.arrayTile[21]= new Tile('V',4);
+            this.arrayTile[22]= new Tile('W',4);
+            this.arrayTile[23]= new Tile('X',8);
+            this.arrayTile[24]= new Tile('Y',4);
+            this.arrayTile[25]= new Tile('Z',10);
+
         }
         private Bag(int[] lettersGame, Tile[] arrayTile) {
 
@@ -101,7 +158,7 @@ public class Tile {
         }
         public void put(Tile t) {
             int position = (int)t.letter - 65;
-            if (checkIfLetterValid(t.letter) && CountTileInArray<=98)
+            if (checkIfLetterValid(t.letter) && CountTileInArray<98)
             {
                 this.LettersGame[position]++;
                 CountTileInArray++;
